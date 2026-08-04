1467a6110fe9225c188839d2a7d786179f3d30b8
Zyl0 <cyprien.tourondecourteix2@gmail.com>
2026-08-04 19:01:48 +0200

    Shader file for previewing meshes in BVH Sample

---
+++ Samples/RayTracingBVH/Shaders/MeshToFrame.glsl
@@ -0,0 +1,70 @@
+#version 430
+
+
+#ifdef VERTEX_SHADER
+layout(location= 0) in vec3 position;
+layout(location= 1) in vec3 normal;
+
+uniform mat4 Model, ViewProjection;
+
+layout(location= 0) out vec3 FragWorldPosition;
+layout(location= 1) out vec3 FragWorldNormal;
+
+vec4 VP(vec4 position)
+{
+    return ViewProjection * position;
+}
+
+vec4 MVP(vec4 position)
+{
+    return VP(Model * position);
+}
+
+vec4 M(vec4 position)
+{
+    return Model * position;
+}
+
+
+void main( )
+{
+    gl_Position = MVP(vec4(position, 1));
+
+    vec4 FragWorldPositionH  = M(vec4(position, 1));
+    FragWorldPosition = FragWorldPositionH.xyz / FragWorldPositionH.w;
+
+    FragWorldNormal = M(vec4(normal, 0)).xyz;
+
+}
+#endif // VERTEX_SHADER
+
+#ifdef FRAGMENT_SHADER
+#include "Include/ToneMapping.glsl"
+
+layout(location= 0) in vec3 FragWorldPosition;
+layout(location= 1) in vec3 FragWorldNormal;
+
+uniform vec3 lightColor;
+uniform vec3 lightDirection;
+uniform vec3 ambientColor;
+
+uniform vec3 baseColor;
+uniform vec3 emissive;
+uniform float metallic;
+uniform float roughness;
+
+out vec4 OutColor;
+
+void main( )
+{    
+    vec3 BaseColor = baseColor;
+    
+    OutColor.xyz = BaseColor * dot(normalize(FragWorldNormal), lightDirection) * lightColor + ambientColor * BaseColor + emissive;
+
+    // sRGB OETF encoding
+    OutColor.xyz = clamp(OutColor.xyz, 0, 1);
+    OutColor.xyz = SRGB_OETF(OutColor.xyz, 2.2 /* PC (Or PC monitor) Gamma, TV is 2.4*/);
+
+    OutColor.w = 1.0;
+}
+#endif // FRAGMENT_SHADER
