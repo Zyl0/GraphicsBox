617aac4c8a20bebd0af28701e122a6376f5a2665
Zyl0 <cyprien.tourondecourteix2@gmail.com>
2026-08-04 18:54:30 +0200

    Added debugging tools for ray tracing

--- Samples/MiniEngine/Include/Modules/Rendering/Tools/DebugModule.h
+++ Samples/MiniEngine/Include/Modules/Rendering/Tools/DebugModule.h
@@ -7,6 +7,8 @@
 #include "Rendering/VertexArrayObject.h"
 
 #include "Core/Module.h"
+#include "Math/Box.h"
+#include "RayTracing/Types.h"
 
 namespace Rendering::Debug
 {
@@ -29,11 +31,23 @@ namespace Rendering::Debug
 
         void DrawFrustum(const Math::Matrix4f& SourceCamera, const Math::Matrix4f& TargetCamera) const;
         void DrawFrustum(const StorageBuffer& CameraBuffer, uint32_t SourceCamera, uint32_t TargetCamera) const;
+        void DrawBox(const Math::Matrix4f& Camera, const Math::Box3f& Box, const Math::Vector3f& Color, const Math::Matrix4f& Transform = Math::MakeMatrix4Identity<float>()) const;
+        void DrawBox(const StorageBuffer& CameraBuffer, uint32_t Camera, const Math::Box3f& Box, const Math::Vector3f& Color, const Math::Matrix4f& Transform = Math::MakeMatrix4Identity<float>()) const;
         void DrawRay(const Math::Matrix4f& WorldToProj, Math::Point3f Origin, float Distance, Math::Vector3f Direction, float HitDistance) const;
         void DrawRay(const StorageBuffer& CameraBuffer, Math::Point3f Origin, float Distance, Math::Vector3f Direction, float HitDistance) const;
 
+        INLINE void DrawRay(const Math::Matrix4f& WorldToProj, const Ray& Ray, float HitDistance)
+        {
+            DrawRay(WorldToProj, Ray.origin, Ray.distance, Ray.direction, HitDistance);
+        }
+        
+        INLINE void DrawRay(const StorageBuffer& CameraBuffer, const Ray& Ray, float HitDistance)
+        {
+            DrawRay(CameraBuffer, Ray.origin, Ray.distance, Ray.direction, HitDistance);
+        }
     private:
         std::optional<Pipeline> m_DrawFrustum;
+        std::optional<Pipeline> m_DrawBox;
         std::optional<Pipeline> m_DrawRay;
         std::optional<VertexArrayObject> m_VAO;
     };
--- Samples/MiniEngine/Source/Modules/Rendering/Tools/DebugModule.cpp
+++ Samples/MiniEngine/Source/Modules/Rendering/Tools/DebugModule.cpp
@@ -19,6 +19,7 @@ namespace Rendering::Debug
     void Module::Initialize()
     {
         m_DrawFrustum.emplace(PipelineFromFile("Debug Frustum", Pipeline::VERTEX_SHADER | Pipeline::FRAGMENT_SHADER, "Debug/Frustum.glsl"));
+        m_DrawBox.emplace(PipelineFromFile("Debug Box", Pipeline::VERTEX_SHADER | Pipeline::FRAGMENT_SHADER, "Debug/Box.glsl"));
         m_DrawRay.emplace(PipelineFromFile("Debug Ray", Pipeline::VERTEX_SHADER | Pipeline::FRAGMENT_SHADER, "Debug/Ray.glsl"));
         m_VAO.emplace();
     }
@@ -30,12 +31,14 @@ namespace Rendering::Debug
     void Module::Shutdown()
     {
         m_DrawFrustum.reset();
+        m_DrawBox.reset();
         m_DrawRay.reset();
         m_VAO.reset();
     }
 
     void Module::DrawFrustum(const Math::Matrix4f& SourceCamera, const Math::Matrix4f& TargetCamera) const
     {
+        glDisable(GL_CULL_FACE);
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         Bind(*m_DrawFrustum);
         Bind(*m_VAO);
@@ -49,10 +52,12 @@ namespace Rendering::Debug
         UnBind(*m_VAO);
         UnBind(*m_DrawFrustum);
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glEnable(GL_CULL_FACE);
     }
 
     void Module::DrawFrustum(const StorageBuffer& CameraBuffer, uint32_t SourceCamera, uint32_t TargetCamera) const
     {
+        glDisable(GL_CULL_FACE);
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         Bind(*m_DrawFrustum);
         Bind(*m_VAO);
@@ -67,11 +72,58 @@ namespace Rendering::Debug
         UnBind(*m_VAO);
         UnBind(*m_DrawFrustum);
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glEnable(GL_CULL_FACE);
+    }
+
+    void Module::DrawBox(const Math::Matrix4f& Camera, const Math::Box3f& Box, const Math::Vector3f& Color, const Math::Matrix4f& Transform) const
+    {
+        glDisable(GL_CULL_FACE);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        Bind(*m_DrawBox);
+        Bind(*m_VAO);
+
+        SetUniform(*m_DrawBox, "useCameraBuffer", false);
+        SetUniform(*m_DrawBox, "CameraWorldToProj", Camera);
+        SetUniform(*m_DrawBox, "Model", Transform);
+        SetUniform(*m_DrawBox, "BoxMin", Box.a);
+        SetUniform(*m_DrawBox, "BoxMax", Box.b);
+        SetUniform(*m_DrawBox, "BoxColor", Color);
+
+        glDrawArrays(GL_QUADS, 0, 24);
+
+        UnBind(*m_VAO);
+        UnBind(*m_DrawBox);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glEnable(GL_CULL_FACE);
+    }
+
+    void Module::DrawBox(const StorageBuffer& CameraBuffer, uint32_t Camera, const Math::Box3f& Box, const Math::Vector3f& Color, const Math::Matrix4f& Transform) const
+    {
+        glDisable(GL_CULL_FACE);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        Bind(*m_DrawBox);
+        Bind(*m_VAO);
+
+        SetUniform(*m_DrawBox, "useCameraBuffer", true);
+        SetUniform(0, CameraBuffer);
+        SetUniform(*m_DrawBox, "Camera", Camera);
+        SetUniform(*m_DrawBox, "Model", Transform);
+        SetUniform(*m_DrawBox, "BoxMin", Box.a);
+        SetUniform(*m_DrawBox, "BoxMax", Box.b);
+        SetUniform(*m_DrawBox, "BoxColor", Color);
+
+        glDrawArrays(GL_QUADS, 0, 24);
+
+        UnBind(*m_VAO);
+        UnBind(*m_DrawBox);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glEnable(GL_CULL_FACE);
     }
 
     void Module::DrawRay(const Math::Matrix4f& WorldToProj, Math::Point3f Origin, float Distance,
-        Math::Vector3f Direction, float HitDistance) const
+                         Math::Vector3f Direction, float HitDistance) const
     {
+        glDisable(GL_CULL_FACE);
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         Bind(*m_DrawRay);
         Bind(*m_VAO);
@@ -88,11 +140,13 @@ namespace Rendering::Debug
         UnBind(*m_VAO);
         UnBind(*m_DrawRay);
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glEnable(GL_CULL_FACE);
     }
 
     void Module::DrawRay(const StorageBuffer& CameraBuffer, Math::Point3f Origin, float Distance,
         Math::Vector3f Direction, float HitDistance) const
     {
+        glDisable(GL_CULL_FACE);
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         Bind(*m_DrawRay);
         Bind(*m_VAO);
@@ -110,5 +164,6 @@ namespace Rendering::Debug
         UnBind(*m_VAO);
         UnBind(*m_DrawRay);
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glEnable(GL_CULL_FACE);
     }
 }
