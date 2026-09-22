c27c6a3c3d3a73b09f1d134eb0ae02b72876b3f0
Zyl0 <cyprien.tourondecourteix2@gmail.com>
2026-09-22 23:39:42 +0200

    Ported Lighting functions from shader code to cpp

---
+++ Samples/MiniEngine/Source/Modules/Rendering/Shaders/Fresnel.cpp
@@ -0,0 +1,80 @@
+﻿#include "Modules/Rendering/Shaders/Fresnel.h"
+
+namespace Rendering
+{
+    float FresnelG(float NdotL, float Refraction)
+    {
+        return sqrt(Refraction * Refraction + NdotL * NdotL - 1);
+    }
+
+    Math::Vector2f FresnelG(float NdotL, Math::Vector2f Refraction)
+    {
+        return Math::Sqrt(Refraction * Refraction + NdotL * NdotL - 1.f);
+    }
+
+    Math::Vector3f FresnelG(float NdotL, Math::Vector3f Refraction)
+    {
+        return Math::Sqrt(Refraction * Refraction + NdotL * NdotL - 1.f);
+    }
+
+    Math::Vector4f FresnelG(float NdotL, Math::Vector4f Refraction)
+    {
+        return Math::Sqrt(Refraction * Refraction + NdotL * NdotL - 1.f);
+    }
+
+    float Fresnel(float NdotL, float F0)
+    {
+        float sqrtF0 = sqrt(F0);
+        float Refraction = (1 + sqrtF0) / (1 - sqrtF0);
+
+        float G = FresnelG(NdotL, Refraction);
+        float C = NdotL;
+
+        float PartA = (G - C) / (G + C);
+        float PartB = (C * (G + C) - 1) / (C * (G + C) + 1);
+
+        return (1 / 2) * (PartA * PartA) * (1 + (PartB * PartB));
+    }
+
+    Math::Vector2f Fresnel(float NdotL, Math::Vector2f F0)
+    {
+        Math::Vector2f sqrtF0 = Sqrt(F0);
+        Math::Vector2f Refraction = (1.f + sqrtF0) / (1.f - sqrtF0);
+
+        Math::Vector2f G = FresnelG(NdotL, Refraction);
+        float C = NdotL;
+
+        Math::Vector2f PartA = (G - C) / (G + C);
+        Math::Vector2f PartB = (C * (G + C) - 1) / (C * (G + C) + 1);
+
+        return (1.f / 2) * (PartA * PartA) * (1.f + (PartB * PartB));
+    }
+
+    Math::Vector3f Fresnel(float NdotL, Math::Vector3f F0)
+    {
+        Math::Vector3f sqrtF0 = Sqrt(F0);
+        Math::Vector3f Refraction = (1.f + sqrtF0) / (1.f - sqrtF0);
+
+        Math::Vector3f G = FresnelG(NdotL, Refraction);
+        float C = NdotL;
+
+        Math::Vector3f PartA = (G - C) / (G + C);
+        Math::Vector3f PartB = (C * (G + C) - 1.f) / (C * (G + C) + 1.f);
+
+        return (1.f / 2) * (PartA * PartA) * (1.f + (PartB * PartB));
+    }
+
+    Math::Vector4f Fresnel(float NdotL, Math::Vector4f F0)
+    {
+        Math::Vector4f sqrtF0 = Sqrt(F0);
+        Math::Vector4f Refraction = (1.f + sqrtF0) / (1.f - sqrtF0);
+
+        Math::Vector4f G = FresnelG(NdotL, Refraction);
+        float C = NdotL;
+
+        Math::Vector4f PartA = (G - C) / (G + C);
+        Math::Vector4f PartB = (C * (G + C) - 1.f) / (C * (G + C) + 1.f);
+
+        return (1.f / 2) * (PartA * PartA) * (1.f + (PartB * PartB));
+    }
+}
---
+++ Samples/MiniEngine/Source/Modules/Rendering/Shaders/LightinModel.cpp
@@ -0,0 +1,24 @@
+﻿#include "Modules/Rendering/Shaders/LightinModel.h"
+
+namespace Rendering
+{
+    Math::Vector3f fDielectrical(const Math::Vector3f& DiffuseColor, float Normalisation, const Math::Vector3f& F)
+    {
+        return ((1.f - F) * DiffuseColor / static_cast<float>(M_PI)) + (F * Normalisation);
+    }
+
+    Math::Vector3f fDielectricalIndirect(const Math::Vector3f& DiffuseColor, float Normalisation, const Math::Vector3f& F)
+    {
+        return F * Normalisation * DiffuseColor;
+    }
+
+    Math::Vector3f fMetallic(float Normalisation, const Math::Vector3f& F)
+    {
+        return ((1.f - F) * Math::Vector3f(0)) + (F * Normalisation);
+    }
+
+    Math::Vector3f fMetallicIndirect(const Math::Vector3f& DiffuseColor, float Normalisation, const Math::Vector3f& F)
+    {
+        return F * Normalisation * Math::Vector3f(1.0f);
+    }
+}
